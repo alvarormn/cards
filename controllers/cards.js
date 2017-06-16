d33dd835dc8fdd2bb2fb1a80e239dd5d5b3b2362
Alvaro <arodriguezm@urbaser.com>
2017-06-16 11:17:08 +0200

    addCards with a function

--- controllers/cards.js
+++ controllers/cards.js
@@ -9,48 +9,42 @@ function createCards(req, res) {
   var suit = ['diamonds','club','spade','hearts'];
   var baraja = [];
 
-  for (var i = 0; i < suit.length; i++) {ç
+  for (var i = 0; i < suit.length; i++) {
 
     newCard.suit = suit[i];
     for (var x = 0; x < num.length; x++) {
       newCard.num = num[x];
       newCard.court = null;
-      //console.log(newCard);
+      console.log(newCard);
 
-      newCard.save((err, cardStored) => {
-        if (err) {
-          console.log('Error al añadir' + err);
-        };
-        if (!cardStored) {
-          console.log('No se ha añadido una carta');
-        }
-        if (cardStored) {
-          console.log(cardStored);
-        }
-      });
+      addCards(newCard);
 
-      /*if (x == num.length - 1) {
+      if (x == num.length - 1) {
         for (var y = 0; y < court.length; y++) {
           newCard.court = court[y];
           newCard.num = null;
           console.log(newCard);
 
-          newCard.save((err, cardStored) => {
-            if (err) {
-              console.log('Error al añadir' + err);
-            };
-            if (!cardStored) {
-              console.log('No se ha añadido una carta');
-            }
-            if (cardStored) {
-              console.log(cardStored);
-            }
-          });
+          addCards(newCard);
 
         }
-      }*/
+      }
     }
   }
+}
+
+function addCards(newCard) {
+  newCard.save((err, cardStored) => {
+    if (err) {
+      console.log('Error al añadir' + err);
+    };
+    if (!cardStored) {
+      console.log('No se ha añadido una carta');
+    }
+    if (cardStored) {
+      console.log(cardStored);
+    }
+  });
 }
 
 module.exports = {createCards};
